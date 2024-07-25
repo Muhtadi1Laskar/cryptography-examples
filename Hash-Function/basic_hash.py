1e8849c9e1390b02b6aed5b1db24cd0046256ab2
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-07-25 19:23:20 +0600

    Added a simple hash 2

--- Hash-Function/basic_hash.py
+++ Hash-Function/basic_hash.py
@@ -8,6 +8,15 @@ def simple_hash(message):
     
     return hash_value
 
+def simple_hash_2(message):
+    hash_value = 0
+    prime = 31
+
+    for elem in message:
+        hash_value = (hash_value * prime + ord(elem)) %  (2**32)
+    
+    return hash_value
+
 
 if __name__ == '__main__':
     data = 'This is a string'
